d23350cbeb0e959aed5169f92c43f9a9ab633c03
dongqunxi <dongqunxi@gmail.com>
2014-12-17 10:44:31 +0100

    dDTF with random permutation

--- test_part.py
+++ test_part.py
@@ -23,51 +23,48 @@ for subject in ['101611','110061','109925', '201394', '202825', '108815']:
     source_localization.ROIs_standardlization(fn_stc_both, size=8)
 
 source_localization.group_ROI()
-source_localization.com_ROI(5)
+source_localization.com_ROI(4)
+source_localization.causal_analysis('108815')
 
-import mne, os
-import numpy as np
-mer_path='/home/qdong/freesurfer/subjects/fsaverage/func_labels/merged'
-list_dirs = os.walk(mer_path)
-label_list = ['']
-for root, dirs, files in list_dirs: 
-    for f in files:
-        label_fname = os.path.join(root, f) 
-        label_list.append(label_fname)
-label_list = label_list[1:]
-class_list = []
-class_list.append(label_list[0]) 
-for test_fn in label_list[1:]:
-    test_label = mne.read_label(test_fn)
-    i = 0
-    belong = False
-    while (i < len(class_list)) and (belong == False):
-        class_label = mne.read_label(class_list[i])
-        label_name = class_label.name
-        if test_label.hemi != class_label.hemi:
-            i = i + 1
-            continue
-        if len(np.intersect1d(test_label.vertices, class_label.vertices)) > 0:
-            com_label = test_label + class_label
-            pre_test = test_label.name.split('_')[0]
-            pre_class = class_label.name.split('_')[0]
-            if pre_test != pre_class:
-                pre_class += ',%s' %pre_test
-                pre_class = list(set(pre_class.split(',')))
-                new_pre = ''
-                for pre in pre_class[:-1]:
-                    new_pre += '%s,' %pre
-                new_pre += pre_class[-1]
-                label_name = '%s_' %new_pre + class_label.name.split('_')[1]
-            if os.path.dirname(class_list[i]) == mer_path:
-                os.remove(class_list[i])
-            if os.path.dirname(test_fn) == mer_path:
-                os.remove(test_fn)
-            mne.write_label(mer_path + '/%s.label' %label_name, com_label)
-            print label_name
-            class_list[i] = mer_path + '/%s.label' %label_name 
-            belong = True
-        i = i + 1
-    if belong == False:
-        class_list.append(test_fn)
-print len(class_list), len(label_list)
+import source_localization, jumeg
+subjects_dir = '/home/qdong/freesurfer/subjects/'
+for subject in ['101611','110061','109925', '201394', '202825']:
+    subject_path = subjects_dir + subject#Set the data path of the subject
+    raw_fname = subject_path + '/MEG/%s_audi_cued-raw.fif' %subject
+    basename = raw_fname.split('-')[0]
+    res_name, tri_name = 'STI 013', 'STI 014'
+    fn_empty_room = subject_path + '/MEG/%s-empty.fif' %subject
+    ############################################
+    # unfiltered interest components extraction#
+    ############################################ 
+    fn_clean_unfilt = basename + ',ar-raw.fif' 
+    fn_ica2_unfilt = basename + ',ar-ica.fif' 
+    fn_ctps_tri_unfilt = basename + ',ar,ctps-trigger.npy'
+    fn_ctps_res_unfilt = basename + ',ar,ctps-response.npy'
+    fn_ics_tri_unfilt = basename + ',ar,ctps-trigger-ic_selection.txt' 
+    jumeg.jumeg_preprocessing.apply_ica(fn_clean_unfilt,n_components=0.95, 
+                                        decim=None)
+    tmin, tmax = 0, 0.3
+    jumeg.jumeg_preprocessing.apply_ctps(fn_ica2_unfilt, tmin=tmin, tmax=tmax, 
+                                                    name_stim=tri_name)
+    jumeg.jumeg_preprocessing.apply_ctps_select_ic(fname_ctps=fn_ctps_tri_unfilt)  
+                                                
+    #response components extraction                                                 
+    tmin, tmax = -0.15, 0.15 
+    jumeg.jumeg_preprocessing.apply_ctps(fn_ica2_unfilt, tmin=tmin, tmax=tmax, 
+                                                    name_stim=res_name)
+    jumeg.jumeg_preprocessing.apply_ctps_select_ic(fname_ctps=fn_ctps_res_unfilt) 
+    #recompose interest components
+    conditions=['trigger', 'response']                
+    jumeg.jumeg_preprocessing.apply_ica_select_brain_response(fn_clean_unfilt, 
+                                    conditions=conditions, n_pca_components=0.95) 
+    
+    ################################################################################
+    # morph the unfiltered and insterest Raw data into the common brain space
+    ###############################################################################
+    fn_unfilt = basename + ',ar,trigger,response,ctpsbr-raw.fif'
+    
+    jumeg.jumeg_preprocessing.apply_create_noise_covariance(fn_empty_room, 
+                                                        require_filter=False)
+    source_localization.make_inverse_epochs(fn_unfilt)
+    source_localization.causal_analysis(subject)
